6f450d893ca44874878556f1963cfb59ebe77a29
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-12-04 15:08:44 -0200

    Business init

---
+++ src/components/Business/Business.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button } from "antd";
+import "./Business.css";
+
+class Business extends Component {
+  render() {
+    return (
+      <div>
+        <Button>aqui</Button>
+      </div>
+    );
+  }
+}
+export default Business;
